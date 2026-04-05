1a4a948fa7f7b0c3639701e330510520664fec3d
marouanesaifhoney <marouanesaifhoney@gmail.com>
2026-04-05 13:40:44 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
  //const API_BASE = process.env.REACT_APP_API_BASE || '/api'
-const API_BASE ='https://ecombackend-staging.up.railway.app/api';
+const API_BASE ='https://backend.up.railway.app/api';
 let memoryToken = ''
 
 export function setAuthToken(token) {
